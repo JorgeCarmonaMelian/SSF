aa9b21f36afa084aafc80823a57c7432c85f17aa
JorgeCarmonaMelian <jorgecarmelian.tarea@gmail.com>
2023-01-19 16:59:03 +0100

    Se crea la estructura de directorios

---
+++ Tareas/Hoja de ejercicios de scripts en GNU Linux y MS Windows/README.md
@@ -0,0 +1,7 @@
+# Hoja de ejercicios de scripts en GNU/Linux y MS Windows
+
+<b>1. Crear un script llamado ej01-resta.sh al que le pasemos dos argumentos y nos devuelva la resta.</b>
+
+```bash
+
+```
---
+++ Tareas/Hoja de ejercicios de scripts en GNU Linux y MS Windows/Scripts/ej01-resta.sh
@@ -0,0 +1,13 @@
+#!/bin/bash/
+
+###################
+#
+# Titulo: ej01-resta.sh
+# Descripcion: Obtiene 2 argumentos y los resta.
+#
+# Autor: Jorge Carmona Melian.
+#
+###################
+
+Valor1=$1
+Valor2=$2
